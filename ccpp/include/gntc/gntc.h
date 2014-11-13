2ef28bf54ab8bc8f068bab423596e68eb8740cd5
cz <cz.theng@gamil.com>
2014-11-13 13:53:44 +0800

    tick

--- ccpp/include/gntc/gntc.h
+++ ccpp/include/gntc/gntc.h
@@ -7,25 +7,35 @@ extern "C"
 #endif
 
 #include <stddef.h>
-struct xxx_t
+struct gntc_line_t
 {
+    char *recvbuf;
+    char *sendbuf;
+    size_t sendbuflen;
+    size_t recvbuflen;
+    size_t sendbufcap;
+    size_t recvbufcap;
     int sfd;
 };
-typedef struct xxx_t xxx;
+typedef struct gntc_line_t gntc_line;
     
-extern int gntc_init();
+extern int gntc_init(gntc_line *line);
 
 extern int gntc_connect(char *url,float timeout);
 
-extern int gntc_disconnect(int hdl);
+extern int gntc_disconnect(gntc_line *line);
 
-extern int gntc_reconnect(int hdl);
+extern int gntc_reconnect(gntc_line *line);
+    
+extern int gntc_set_recvbuf(gntc_line *line,size_t len);
+    
+extern int gntc_set_sendbuf(gntc_line *line,size_t len);
 
-extern int gntc_sendbuf(int hdl,const void *buf, size_t len, float timeout);
+extern int gntc_sendbuf(gntc_line *line,const void *buf, size_t len, float timeout);
 
-extern int gntc_recvbuf(int hdl, void *buf, size_t len,float timeout);
+extern int gntc_recvbuf(gntc_line *line, void *buf, size_t len,float timeout);
 
-extern int gntc_uninit(int hdl);
+extern int gntc_uninit(gntc_line *line);
 
 #ifdef __cplusplus
 }
--- ccpp/src/gntc.c
+++ ccpp/src/gntc.c
@@ -4,37 +4,99 @@
 #include "pal/gnt_socket.h"
 #include "pal/gnt_os.h"
 
-int gntc_init()
+
+int gntc_init(gntc_line *line)
 {
+    line->recvbuf = line->sendbuf = NULL;
+    line->recvbufcap = line->recvbuflen = 0;
+    line->sendbufcap = line->sendbuflen = 0;
+    line->sfd = -1;
     return 0;
 }
 
 int gntc_connect(char *url,float timeout)
 {
+    // first do connect to server with socket
+    
+    // second send connect request and recive response
+    
+    // check if invalied
+    
+    return -1;
+}
+
+int gntc_disconnect(gntc_line *line)
+{
+    // first send disconnect request
+    
+    // close fd
+    
+    return -1;
+}
+
+int gntc_reconnect(gntc_line *line)
+{
+    // first send reconnect request
+    
+    // deal with response
+    
+    return -1;
+}
+
+int gntc_set_recvbuf(gntc_line *line,size_t len)
+{
+    if (line->recvbufcap >= len){
+        line->recvbufcap = len;
+    } else {
+        char *buf =realloc(line->recvbuf,len);
+        if (buf == NULL) {
+            return -1;
+        }
+        line->recvbuf = buf;
+        line->recvbufcap = len;
+    }
+    
     return 0;
 }
 
-int gntc_disconnect(int hdl)
+int gntc_set_sendbuf(gntc_line *line,size_t len)
 {
+    if (line->sendbufcap >= len){
+        line->sendbufcap = len;
+    } else {
+        char *buf =realloc(line->sendbuf,len);
+        if (buf == NULL) {
+            return -1;
+        }
+        line->sendbuf = buf;
+        line->sendbufcap = len;
+    }
+    
     return 0;
 }
 
-int gntc_reconnect(int hdl)
+int gntc_sendbuf(gntc_line *line,const void *buf, size_t len, float timeout)
 {
-    return 0;
+    return -1;
 }
 
-int gntc_sendbuf(int hdl,const void *buf, size_t len, float timeout)
+int gntc_recvbuf(gntc_line *line, void *buf, size_t len,float timeout)
 {
-    return 0;
+    return -1;
 }
 
-int gntc_recvbuf(int hdl, void *buf, size_t len,float timeout)
+int gntc_uninit(gntc_line *line)
 {
-    return 0;
-}
-
-int gntc_uninit(int hdl)
-{
+    if (line->recvbuf != NULL) {
+        free(line->recvbuf);
+    }
+    line->recvbuf  = NULL;
+    
+    if (line->sendbuf != NULL) {
+        free(line->sendbuf);
+    }
+    line->sendbuf = NULL;
+    line->sendbufcap = line->sendbuflen = 0;
+    line->recvbufcap = line->recvbuflen = 0;
     return 0;
 }
